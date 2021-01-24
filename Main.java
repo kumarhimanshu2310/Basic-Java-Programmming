63cabb9598e0eca9033cfb69ab55e32cca389f21
kumarhimanshu2310 <64266898+kumarhimanshu2310@users.noreply.github.com>
2021-01-25 00:37:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// area of rectangle first
+        Rectangle R1=new Rectangle();
+        R1.SetValues(10,2,0);
+        System.out.println("Area of rectangle is "+ R1.AreaOfRectangle());
+    }
+}
